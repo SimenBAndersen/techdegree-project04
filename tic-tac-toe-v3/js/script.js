8db68d8543f914f58c547e2c87de642a79b0a97e
Simen Bjerkeset Andersen <simen-bandersen@hotmail.no>
2017-09-18 14:49:57 +0200

    minor changes
    modular function

---
+++ tic-tac-toe-v3/js/script.js
@@ -0,0 +1,9 @@
+(function() {
+
+  // Start page
+  document.body.innerHTML = "<div class='screen screen-start' id='start'>" +
+    "<header><h1>Tic Tac Toe</h1><a href=''#'' class='button'>Start game" +
+    "</a></header></div>";
+
+
+}());
