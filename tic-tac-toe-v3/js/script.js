c5f8626762a7f23b35078e5c82b508bb1ae6a72d
Simen Bjerkeset Andersen <simen-bandersen@hotmail.no>
2017-09-18 15:23:13 +0200

    start and board screen
    Shows start screen and changes to the board when the start game button is clicked.

--- tic-tac-toe-v3/js/script.js
+++ tic-tac-toe-v3/js/script.js
@@ -1,9 +1,19 @@
 (function() {
 
-  // Start page
-  document.body.innerHTML = "<div class='screen screen-start' id='start'>" +
-    "<header><h1>Tic Tac Toe</h1><a href=''#'' class='button'>Start game" +
-    "</a></header></div>";
+  // Selectors
+  const start = document.querySelector("#start");
+  const board = document.querySelector("#board");
+  const finish = document.querySelector("#finish");
+  const startButton = document.querySelector("#start .button");
 
+  // Disable the bord and finish HTML before game is started
+  board.style.display = "none";
+  finish.style.display = "none";
+
+  // When the start game button is clicked: Switch from start to boards screen
+  startButton.onclick = function () {
+    start.style.display = "none";
+    board.style.display = "block";
+  };
 
 }());
